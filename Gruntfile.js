db6ff251554f6d56e1f9d082fdb23084e898a281
Atanas Atanasov <nascovt@outlook.com>
2015-11-16 09:25:54 +0200

    added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            dist: {
+                files : {
+                    'dialog/build/dialog.base.js': ['dialog/src/dialog.base.config.js', 'dialog/src/dialog.base.events.js', 'dialog/src/dialog.base.private.js', 'dialog/src/dialog.base.methods.js']
+                }
+            }
+        },
+        copy: {
+            options: {},
+            files: {
+                'dialog/build/': ['dialog/src/dialog.jqueryui.js', 'dialog/src/dialog.foundation.js']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'copy']);
+
+};
