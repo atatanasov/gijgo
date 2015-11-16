844c52317c9cf5792f40e00afeeeca4a8ba19e59
Atanas Atanasov <nascovt@outlook.com>
2015-11-16 11:15:14 +0200

    added new tasks in the grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,24 +4,65 @@
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         concat: {
-            dist: {
+            dialog: {
                 files : {
-                    'dialog/build/dialog.base.js': ['dialog/src/dialog.base.config.js', 'dialog/src/dialog.base.events.js', 'dialog/src/dialog.base.private.js', 'dialog/src/dialog.base.methods.js']
+                    'dialog/build/dialog.base.js': ['dialog/src/dialog.base.config.js', 'dialog/src/dialog.base.events.js', 'dialog/src/dialog.base.private.js', 'dialog/src/dialog.base.methods.js'],
+                    'dialog/build/dialog.jqueryui.js': ['dialog/src/dialog.jqueryui.js'],
+                    'dialog/build/dialog.foundation.js': ['dialog/src/dialog.foundation.js'],
+                    'dialog/build/dialog.all.js': ['dialog/build/dialog.base.js', 'dialog/build/dialog.jqueryui.js', 'dialog/build/dialog.foundation.js']
                 }
             }
         },
-        copy: {
+        replace: {
+            dialog: {
+                src: ['dialog/build/*.js'],      // source files array (supports minimatch) 
+                overwrite: true,                 // overwrite matched source files 
+                replacements: [{
+                    from: /(.|\n|\r)+/g,
+                    to: function (matchedWord, index, fullText, regexMatches) {
+                        var i,
+                            isCommentMode = false,
+                            isExtraCommentMode = false,
+                            result = '',
+                            lines = fullText.split('\n');
+                        for (i = 0; i < lines.length; i++) {
+                            if (/\/\*\*/g.test(lines[i])) {
+                                isCommentMode = true;
+                            } else if (/\*.?@/g.test(lines[i])) {
+                                isExtraCommentMode = true;
+                            }
+                            if (isExtraCommentMode === false) {
+                                result += lines[i] + '\n';
+                            }
+                            if (isCommentMode && /\*\//g.test(lines[i])) {
+                                isCommentMode = false;
+                                isExtraCommentMode = false;
+                                result = result.replace(/\n$/, "") + ' */\n'
+                            }
+                        }
+                        return result;
+                    }
+                }]
+            }
+        },
+        uglify: {
             options: {},
-            files: {
-                'dialog/build/': ['dialog/src/dialog.jqueryui.js', 'dialog/src/dialog.foundation.js']
+            dialog: {
+                files: {
+                    'dialog/build/dialog.all.min.js': ['dialog/build/dialog.all.js'],
+                    'dialog/build/dialog.base.min.js': ['dialog/build/dialog.base.js'],
+                    'dialog/build/dialog.jqueryui.min.js': ['dialog/build/dialog.jqueryui.js'],
+                    'dialog/build/dialog.foundation.min.js': ['dialog/build/dialog.foundation.js']
+                }
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-copy');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-text-replace');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     // Default task(s).
-    grunt.registerTask('default', ['concat', 'copy']);
+    grunt.registerTask('default', ['concat', 'replace', 'uglify']);
 
 };
