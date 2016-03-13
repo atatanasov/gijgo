0ed57f26417da969e85c3a0c6089a73992e7161f
Atanas Atanasov <nascovt@outlook.com>
2016-03-13 10:54:48 +0200

    added rowReorder grid plugin

---
+++ src/grid/js/grid.rowReorder.js
@@ -0,0 +1,99 @@
+﻿/** 
+  * @widget Grid 
+  * @plugin rowReorder
+  */
+if (typeof (gj.grid.plugins) === 'undefined') {
+    gj.grid.plugins = {};
+}
+
+gj.grid.plugins.rowReorder = {
+    config: {
+        base: {
+            /** If set to true, enable row reordering with drag and drop.
+             * @type boolean
+             * @default false
+             * @example sample <!-- grid.base, draggable.base, grid.rowReorder -->
+             * <table id="grid"></table>
+             * <script>
+             *     $('#grid').grid({
+             *         dataSource: '/DataSources/GetPlayers',
+             *         rowReorder: true,
+             *         columns: [ { field: 'ID' }, { field: 'Name' }, { field: 'PlaceOfBirth' } ]
+             *     });
+             * </script>
+             */
+            rowReorder: false,
+
+            rowReorderColumn: undefined,
+
+            orderNumberField: undefined,
+
+            style: {
+                targetRowIndicatorTop: 'gj-grid-row-reorder-indicator-top',
+                targetRowIndicatorBottom: 'gj-grid-row-reorder-indicator-bottom'
+            }
+        }
+    },
+
+    private: {
+        init: function ($grid) {
+            var i, $rows = $grid.find('tbody tr[data-role="row"]');
+            for (i = 0; i < $rows.length; i++) {
+                $($rows[i]).on('mousedown', gj.grid.plugins.rowReorder.private.createRowMouseDownHandler($grid));
+                $($rows[i]).on('mousedown', gj.grid.plugins.rowReorder.private.createRowMouseUpHandler($grid));
+                $($rows[i]).on('mouseover', gj.grid.plugins.rowReorder.private.createRowMouseOverHandler($grid));
+                $($rows[i]).on('mouseout', gj.grid.plugins.rowReorder.private.createRowMouseOutHandler($grid));
+            }
+        },
+
+        createRowMouseDownHandler: function ($grid) {
+            return function (e) {
+                var $tr = $(this),
+                    $dragEl = $grid.clone();
+                $('body').append($dragEl);
+                $dragEl.attr('data-role', 'draggable-clone');
+                $dragEl.children('thead').remove().children('tfoot').remove();
+                $dragEl.find('tbody tr:not([data-position="' + $tr.data('position') + '"])').remove();
+                $dragEl.draggable({ stop: function () { $(this).remove(); } });
+                $dragEl.css({ position: 'absolute', top: $tr.offset().top, left: $tr.offset().left, width: $tr.width() });
+                $dragEl.trigger('mousedown');
+                $grid.attr('data-row-reorder-position', $tr.data('position'));
+            };
+        },
+
+        createRowMouseOverHandler: function ($grid) {
+            return function (e) {
+                var $tr = $(this);
+                $tr.addClass('gj-grid-base-bottom-border');
+                $tr.attr('data-row-reorder-target', true);
+            };
+        },
+
+        createRowMouseOutHandler: function ($grid) {
+            return function (e) {
+                var $tr = $(this);
+                $tr.removeClass('gj-grid-base-bottom-border');
+                $tr.attr('data-row-reorder-target', false);
+            };
+        },
+
+        createRowMouseUpHandler: function ($grid) {
+            return function (e) {
+                var $tr = $(this);
+                if ($tr.attr('data-row-reorder-target')) {
+                    alert('move');
+                }
+            };
+        }
+    },
+
+    public: {
+    },
+
+    configure: function ($grid) {
+        $.extend(true, $grid, gj.grid.plugins.rowReorder.public);
+        $grid.on('dataBound', function () {
+            gj.grid.plugins.rowReorder.private.init($grid);
+        });
+    }
+};
