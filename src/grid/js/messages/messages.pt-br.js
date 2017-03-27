4dad755a4932a78f2edf6982bbc5341891439623
Atanas Atanasov <nascovt@outlook.com>
2017-03-27 11:00:07 +0300

    added pt-br translation for the grid

--- src/grid/js/messages/messages.pt-br.js
+++ src/grid/js/messages/messages.pt-br.js
@@ -1,1 +1,19 @@
-﻿
+﻿gj.grid.messages['pt-br'] = {
+    First: 'Primeiro',
+    Previous: 'Anterior',
+    Next: 'Próximo',
+    Last: 'Último',
+    Page: 'Página',
+    FirstPageTooltip: 'Primeira página',
+    PreviousPageTooltip: 'Página anterior',
+    NextPageTooltip: 'Próxima página',
+    LastPageTooltip: 'Última Página',
+    Refresh: 'Atualizar',
+    Of: 'de',
+    DisplayingRecords: 'Mostrando registros',
+    Edit: 'Editar',
+    Delete: 'Excluir',
+    Update: 'Alterar',
+    Cancel: 'Cancelar',
+    NoRecordsFound: 'Nenhum registro encontrado.'
+};
