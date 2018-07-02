0295f7142fbac47a97128f02711fb1eb390de103
Tamer Ağaoğlu <farerock@gmail.com>
2018-07-02 21:51:17 +0300

    Create messages.tr-tr.js
    Türkçe Dil Dosyası

---
+++ dist/combined/js/messages/messages.tr-tr.js
@@ -0,0 +1,52 @@
+gj.dialog.messages['tr-tr'] = {
+    Close: 'Kapat',
+    DefaultTitle: 'Diyalog'
+};
+gj.grid.messages['tr-tr'] = {
+    First: 'İlk',
+    Previous: 'Önceki',
+    Next: 'Sonraki',
+    Last: 'Son',
+    Page: 'Sayfa',
+    FirstPageTooltip: 'İlk sayfa',
+    PreviousPageTooltip: 'Önceki sayfa',
+    NextPageTooltip: 'Sonraki sayfa',
+    LastPageTooltip: 'Son sayfa',
+    Refresh: 'Yenile',
+    Of: 'üzerinden',
+    DisplayingRecords: 'Sonuçlar',
+    RowsPerPage: 'Lignes par page:',
+    Edit: 'Düzenle',
+    Delete: 'Sil',
+    Update: 'Güncelle',
+    Cancel: 'İptal',
+    NoRecordsFound: 'Kayıt Yok.',
+    Loading: 'Yükleniyor...'
+};
+gj.editor.messages['tr-tr'] = {
+    bold: 'Kalın',
+    italic: 'İtalik',
+    strikethrough: 'Çok Kalın',
+    underline: 'Altı Çizgili',
+    listBulleted: 'Noktalı',
+    listNumbered: 'Rakamlı',
+    indentDecrease: 'Girintiyi Azalt',
+    indentIncrease: 'Girintiyi Çoğalt',
+    alignLeft: 'Sola Yasla',
+    alignCenter: 'Ortala',
+    alignRight: 'Sağ Yasla',
+    alignJustify: 'Yay',
+    undo: 'Geri Al',
+    redo: 'İleri Al'
+};
+gj.core.messages['tr-tr'] = {
+    monthNames: ['ocak', 'şubat', 'mart', 'nisan', 'mayıs', 'haziran', 'temmuz', 'ağustos', 'eylül', 'ekim', 'kasım', 'aralık'],
+    monthShortNames: ['oca', 'şub', 'mar', 'nis', 'may', 'haz', 'tem', 'ağu', 'eyl', 'eki', 'kas', 'ara'],
+    weekDaysMin: ['P', 'S', 'Ç', 'P', 'C', 'C', 'P'],
+    weekDaysShort: ['pzt.', 'sl.', 'çar.', 'per.', 'cum.', 'cmt.', 'paz.'],
+    weekDays: ['pazartesi', 'salı', 'çarşamba', 'perşembe', 'cuma', 'cumartesi', 'pazar'],
+    am: 'AM',
+    pm: 'PM',
+    ok: 'Tamam',
+    cancel: 'İptal'
+};
