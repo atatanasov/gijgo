501a1defff392e19259432211de03f3c8dc4298c
Pavel Kovář <kovarp95@gmail.com>
2018-10-17 16:36:00 +0200

    messages.cs-cz.js created

---
+++ src/core/js/messages/messages.cs-cz.js
@@ -0,0 +1,11 @@
+﻿gj.core.messages['cs-cz'] = {
+    monthNames: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
+    monthShortNames: ['Led', 'Úno', 'Bře', 'Dub', 'Kvě', 'Čvn', 'Čvc', 'Srp', 'Zář', 'Říj', 'Lis', 'Pro'],
+    weekDaysMin: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'],
+    weekDaysShort: ['Ne', 'Po', 'Út', 'St', 'Čt', 'Pá', 'So'],
+    weekDays: ['Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota'],
+    am: 'AM',
+    pm: 'PM',
+    ok: 'OK',
+    cancel: 'Zavřít'
+};
