d93ff1a174c1adfacb0ef96e69513055deb41f18
Bogdan Dovgopol <bogdandovgopol@mac-zyx.local>
2019-03-30 11:05:30 +0100

    add support for Czech language

---
+++ dist/combined/js/messages/messages.cs-cz.js
@@ -0,0 +1,54 @@
+gj.dialog.messages['cs-cz'] = {
+    Close: 'Zavřít',
+    DefaultTitle: 'Zpráva'
+};
+gj.grid.messages['cs-cz'] = {
+    First: 'První',
+    Previous: 'Předchozí',
+    Next: 'Další',
+    Last: 'Poslední',
+    Page: 'Stránka',
+    FirstPageTooltip: 'První stránka',
+    PreviousPageTooltip: 'Předchozí stránka',
+    NextPageTooltip: 'Další stránka',
+    LastPageTooltip: 'Poslední stránka',
+    Refresh: 'Obnovit',
+    Of: 'od',
+    DisplayingRecords: 'Zobrazit záznamy',
+    RowsPerPage: 'Řádků na stránku:',
+    Edit: 'Upravit',
+    Delete: 'Smazat',
+    Update: 'Obnovit',
+    Cancel: 'Zrušit',
+    NoRecordsFound: 'Nebyly nalezeny žádné záznamy.',
+    Loading: 'Načítání...'
+};
+gj.editor.messages['cs-cz'] = {
+	bold: 'Tučný',
+	italic: 'Kurzíva',
+	strikethrough: 'Přeškrtnutý',
+	underline: 'Podtrhnutý',
+	listBulleted: 'Seznam',
+	listNumbered: 'Číslovaný seznam',
+	indentDecrease: 'Zmenšit odsazení',
+	indentIncrease: 'Zvětšit odsazení',
+	alignLeft: 'Zarovnat doleva',
+	alignCenter: 'Zarovnat na střed',
+	alignRight: 'Zarovnat doprava',
+	alignJustify: 'Zarovnat po šířce',
+	undo: 'Zpět',
+	redo: 'Další'
+};
+gj.core.messages['cs-cz'] = {
+    monthNames: ['Leden', 'Únor', 'Březen', 'Duben', 'Květen', 'Červen', 'Červenec', 'Srpen', 'Září', 'Říjen', 'Listopad', 'Prosinec'],
+    monthShortNames: ['Led', 'Úno', 'Bře', 'Dub', 'Kvě', 'Čvn', 'Čvc', 'Srp', 'Zář', 'Říj', 'Lis', 'Pro'],
+    weekDaysMin: ['ne', 'po', 'út', 'st', 'čt', 'pá', 'so'],
+    weekDaysShort: ['ne', 'po', 'út', 'st', 'čt', 'pá', 'so'],
+    weekDays: ['Neděle', 'Pondělí', 'Úterý', 'Středa', 'Čtvrtek', 'Pátek', 'Sobota'],
+    am: 'AM',
+    pm: 'PM',
+    ok: 'OK',
+    cancel: 'Zrušit',
+    titleFormat: 'mmmm yyyy'
+
+};
