e9e90b0d688487d52d4db99cdc4eda5899d5c939
Diego Durán <46654166+diegodurrod@users.noreply.github.com>
2019-08-29 13:59:35 +0200

    Fixed Wednesday acronym

--- src/core/js/messages/messages.es-es.js
+++ src/core/js/messages/messages.es-es.js
@@ -1,7 +1,7 @@
 ﻿gj.core.messages['es-es'] = {
     monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
     monthShortNames: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
-    weekDaysMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+    weekDaysMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
     weekDaysShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
     weekDays: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
     am: 'AM',
