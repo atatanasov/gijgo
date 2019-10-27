ba810e736ef10eda5db0ab3cfa76ae276d8b3c5d
Adrian Voicu <avenir.ro@gmail.com>
2019-10-27 18:45:16 +0200

    Messages in Romanian

---
+++ src/core/js/messages/messages.ro-ro.js
@@ -0,0 +1,12 @@
+gj.core.messages['ro-ro'] = {
+    monthNames: ['ianuarie', 'februarie', 'martie', 'aprilie', 'mai', 'iunie', 'iulie', 'august', 'septembrie', 'octombrie', 'noiembrie', 'decembrie'],
+    monthShortNames: ['ian.', 'feb.', 'mar.', 'apr.', 'mai', 'iun.', 'iul.', 'aug.', 'sept.', 'oct.', 'noi.', 'dec.'],
+    weekDaysMin: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
+    weekDaysShort: ['dum.', 'lun.', 'mar.', 'mie.', 'joi', 'vin.', 'sâm.'],
+    weekDays: ['duminică', 'luni', 'marţi', 'miercuri', 'joi', 'vineri', 'sâmbătă'],
+    am: 'AM',
+    pm: 'PM',
+    ok: 'OK',
+    cancel: 'Anulează',
+    titleFormat: 'mmmm yyyy'
+};
